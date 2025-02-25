db82c752de66d4b743c251ac8a4f99e5d878db7c
Wendy Balderrama <balderramawendym@gmail.com>
2025-02-24 23:59:44 -0400

    Numero 2 a romano

--- src/convertidor.js
+++ src/convertidor.js
@@ -1,6 +1,8 @@
 function convertidor(num) {
     if(num===1){
         return "I";
+    }else if(num===2){
+        return "II";
     }
   }
   
--- src/convertidor.test.js
+++ src/convertidor.test.js
@@ -1,7 +1,11 @@
 import convertidor from "./convertidor.js";
 
-describe("Convertidor de romanos: ", () => {
-  it("deberia convertir 1 en I: ", () => {
+describe("Convertidor", () => {
+  it("deberia convertir numero a 1 a romano", () => {
     expect(convertidor(1)).toEqual("I");
+  });
+
+  it("deberia convertir numero a 2 a romano", () => {
+    expect(convertidor(2)).toEqual("II");
   });
 });
